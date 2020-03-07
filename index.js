e1fc3fe8bf0de97d04868d29416460cc4b02f6b3
Gufran Mirza <gufranmirza1@gmail.com>
2020-03-08 00:20:12 +0530

    init + 1

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = function(lang) {
-  if(lang === "es") {
-    console.log('Hola Marte!!')
-  } else {
-    console.log('Hello Mars!!')
-  }
+module.exports = function() {
+  console.log('Hello Mars!!')
 }
